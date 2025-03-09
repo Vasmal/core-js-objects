eb1505af7a6fb0ef5e4695a1bca13f8bceb81ebf
Vasilii Malai <malaivasili@gmail.com>
2025-03-09 20:39:32 +0300

    feat: add fromJSON function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -217,8 +217,12 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const data = JSON.parse(json);
+  const instance = Object.create(proto.constructor.prototype);
+  proto.constructor.apply(instance, Object.values(data));
+
+  return instance;
 }
 
 /**
